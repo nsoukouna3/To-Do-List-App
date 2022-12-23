16b1409ab4ab8e7db3a1d142e85c5ab85bb35b91
Nama Soukouna <nsoukouna3@gmail.com>
2022-12-23 16:51:45 -0500

    implementing templating

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
 const Express= require("express");
 const bodyParser = require("body-parser");
 
+
 const app =Express();
+app.set("view engine", "ejs");
 
 
 app.get("/", function(req,res){
     let today = new Date();
     let currentDay= today.getDay();
-    if(currentDay==6|| currentDay==0){
-        res.write("<h1>Yay it's the weekend</h1>");
+    let day="";
+
+    switch(currentDay){
+        case 0:
+            day="Sunday";
+            break;
+        case 1:
+            day="Monday";
+            break;
+        case 2:
+            day="Tuesday";
+            break;
+        case 3:
+            day="Wednesday";
+            break;
+        case 4:
+            day="Thursday";
+            break;
+        case 5:
+            day="Friday";
+            break;
+        case 6:
+            day="Saturday";
+            break;
+        default:
+            console.log("Error: current day is equal to: "+ currentDay)
+            Break;
     }
-    else{
-   
-        res.sendFile(__dirname+"/index.html");
-    }
+    
+    res.render("list", {kindOfDay:day});
 
 });
 
