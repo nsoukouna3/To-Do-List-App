968be45ba61a9d4c048e75431f19a6e7fd3123f7
Nama Soukouna <nsoukouna3@gmail.com>
2022-12-23 19:10:51 -0500

    implementing the cart list

--- app.js
+++ app.js
@@ -5,42 +5,30 @@ const bodyParser = require("body-parser");
 const app =Express();
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({extended:true}));
 
+let items=["Buy Food", "Cook Food", "Eat Food"];
 app.get("/", function(req,res){
     let today = new Date();
-    let currentDay= today.getDay();
-    let day="";
+    let options ={
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+    };
 
-    switch(currentDay){
-        case 0:
-            day="Sunday";
-            break;
-        case 1:
-            day="Monday";
-            break;
-        case 2:
-            day="Tuesday";
-            break;
-        case 3:
-            day="Wednesday";
-            break;
-        case 4:
-            day="Thursday";
-            break;
-        case 5:
-            day="Friday";
-            break;
-        case 6:
-            day="Saturday";
-            break;
-        default:
-            console.log("Error: current day is equal to: "+ currentDay)
-            Break;
-    }
+    let day= today.toLocaleDateString("en-US", options);
     
-    res.render("list", {kindOfDay:day});
+    res.render("list", {kindOfDay:day, newListItem:items});
 
 });
+
+app.post("/", (req, res)=>{
+   let item = req.body.newItem;   // new item come from the name in the form for input
+    
+    items.push(item);
+    res.redirect("/");
+
+})
 
 
 app.listen(3000,function(){
