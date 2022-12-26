1b1fa2ebede27d7064079ba611eba85f8726ac6b
Nama Soukouna <nsoukouna3@gmail.com>
2022-12-26 14:46:12 -0500

    style the pages

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const Express= require("express");
+const express= require("express");
 const bodyParser = require("body-parser");
 
 
-const app =Express();
+const app =express();
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 let items=["Buy Food", "Cook Food", "Eat Food"];
 app.get("/", function(req,res){
